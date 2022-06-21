69426e1fbcd9f4a934dc34b903a7da9ba4c4b90d
nicol.buratti <nicol.buratti@studenti.unicam.it>
2022-06-21 20:44:47 +0200

    App completata, migliorare struttura

--- app/src/main/java/it/unicam/cs/pa/logo/app/FileExecution.java
+++ app/src/main/java/it/unicam/cs/pa/logo/app/FileExecution.java
@@ -4,7 +4,6 @@ import it.unicam.cs.pa.logo.Controller;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.util.Scanner;
 
 /**
@@ -14,7 +13,7 @@ public class FileExecution {
 
     public void execute(Controller controller, Scanner scanner) throws IOException {
         System.out.println("Hai scelto l'esecuzione da un file\nInserire nome del file (deve essere nella stessa cartella del programma)");
-        String script = Files.readString(new File(scanner.next()).toPath());
+        String script = controller.read(new File(scanner.next()));
         try {
             controller.computeScript(script);
         } catch (IOException e) {
