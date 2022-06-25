51f1e79b3d29f9b1ed69c061d0aeabbf48302c2f
Nicol Buratti <nickibrt8@gmail.com>
2022-06-25 18:25:21 +0200

    Aggiunto test mancante

--- list/src/test/java/it/unicam/cs/pa/logo/model/ShapeTest.java
+++ list/src/test/java/it/unicam/cs/pa/logo/model/ShapeTest.java
@@ -1,7 +1,9 @@
 package it.unicam.cs.pa.logo.model;
 
+import it.unicam.cs.pa.logo.model.defined.TwoDimCoordinate;
 import it.unicam.cs.pa.logo.model.defined.TwoDimDrawer;
 import it.unicam.cs.pa.logo.model.defined.TwoDimEnvironment;
+import it.unicam.cs.pa.logo.model.defined.TwoDimSegment;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -26,4 +28,22 @@ class ShapeTest {
         drawer.drawLine(environment,90);
        assertEquals(2, environment.getShapes().size());
     }
+
+    @Test
+    void drawLine() {
+        TwoDimSegment s1 = new TwoDimSegment(new TwoDimCoordinate(50, 50), new TwoDimCoordinate(60, 60));
+        drawer.drawLine(environment,s1);
+        TwoDimSegment s2 = new TwoDimSegment(new TwoDimCoordinate(60, 60), new TwoDimCoordinate(70, 70));
+        drawer.drawLine(environment,s2);
+        environment.getCursor().setPlot(false);
+        TwoDimSegment s3 = new TwoDimSegment(new TwoDimCoordinate(70, 70), new TwoDimCoordinate(80, 80));
+        drawer.drawLine(environment,s3);
+        assertEquals(1, environment.getShapes().size());
+        assertEquals(2, environment.getShapes().get(0).numSegments());
+        environment.getCursor().setPlot(true);
+        TwoDimSegment s4 = new TwoDimSegment(new TwoDimCoordinate(80, 80), new TwoDimCoordinate(90, 90));
+        drawer.drawLine(environment,s4);
+        assertEquals(2, environment.getShapes().size());
+    }
+
 }
